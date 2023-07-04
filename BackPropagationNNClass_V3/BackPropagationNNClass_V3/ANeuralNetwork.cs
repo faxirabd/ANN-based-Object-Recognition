15607b4bb9f6ab3e796252402a22953e746a5cff
faxir <115953037+faxirabd@users.noreply.github.com>
2023-07-04 23:46:05 +0100

    Add files via upload

---
+++ BackPropagationNNClass_V3/BackPropagationNNClass_V3/ANeuralNetwork.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackPropagationNNClass_V3
+{
+    class ANeuralNetwork
+    {
+        private int inputCount;
+        private int hiddenCount;
+        private int outputCount;
+
+        private double[] inputs;
+        private double[,] iToHWeights; // input-to-hidden weights
+        private double[] iToHSums;
+        private double[] iToHBiases;
+        private double[] iToHOutputs;
+
+        private double[,] hToOutWeights;  // hidden-to-output
+        private double[] hToOutSums;
+        private double[] hToOutBiases;
+        private double[] outputs;
+
+        private double[] outGrads; // output gradients for back-propagation
+        private double[] hiddenGrads; // hidden gradients for back-propagation
+
+        private double[,] iToHPrevDeltaWeights;  // for momentum with back-propagation
+        private double[] iToHPrevDeltaBiases; // for momentum with back-propagation
+
+        private double[,] hToOutPrevDeltaWeights; // for momentum with back-propagation
+        private double[] hToOutPrevDeltaBiases; // for momentum with back-propagation
+
+        public ANeuralNetwork(int inputCount, int hiddenCount, int outputCount)
+        {
+            this.inputCount = inputCount;
+            this.hiddenCount = hiddenCount;
+            this.outputCount = outputCount;
+
+            inputs = new double[inputCount];
+            iToHWeights = new double[inputCount, hiddenCount];
+            iToHSums = new double[hiddenCount];
+            iToHBiases = new double[hiddenCount];
+            iToHOutputs = new double[hiddenCount];
+            hToOutWeights = new double[hiddenCount, outputCount];
+            hToOutSums = new double[outputCount];
+            hToOutBiases = new double[outputCount];
+            outputs = new double[outputCount];
+
+            outGrads = new double[outputCount];
+            hiddenGrads = new double[hiddenCount];
+
+            iToHPrevDeltaWeights = new double[inputCount, hiddenCount];
+            iToHPrevDeltaBiases = new double[hiddenCount];
+            hToOutPrevDeltaWeights = new double[hiddenCount, outputCount];
+            hToOutPrevDeltaBiases = new double[outputCount];
+        }
+
+        // change the weights and biases using back-propagation, with desired values, eta (learning rate), alpha (momentum)
+        public void UpdateWeights(double[] desiredValues, double eta, double alpha) 
+        {
+            // 1. compute output gradients
+            for (int i = 0; i < outGrads.Length; ++i)
+            {
+                double derivative = (1 - outputs[i]) * (1 + outputs[i]); // derivative of tanh
+                outGrads[i] = derivative * (desiredValues[i] - outputs[i]); //equations 4.17 from EBook
+            }
+
+            // 2. compute hidden gradients
+            for (int i = 0; i < hiddenGrads.Length; ++i)
+            {
+                double derivative = (1 - iToHOutputs[i]) * iToHOutputs[i]; // (1 / 1 + exp(-x))' Equations 4.19 from EBook
+                double sum = 0.0;
+                //The error signal for a hidden unit is determined recursively in terms of error signals of the
+                //units to which it directly connects and the weights of those connections. For the sigmoid
+                //activation function:
+                //Equations 4.21 from EBook
+                for (int j = 0; j < outputCount; ++j) // each hidden delta is the sum of numOutput terms
+                    sum += outGrads[j] * hToOutWeights[i,j]; // each downstream gradient * outgoing weight
+               hiddenGrads[i] = derivative * sum;
+            }
+
+            // 3. update input to hidden weights
+            for (int i = 0; i < iToHWeights.GetLength(0); ++i) 
+            {
+                for (int j = 0; j < iToHWeights.GetLength(1); ++j) 
+                {
+                    double delta = eta * hiddenGrads[j] * inputs[i]; //compute the new delta equation 4.16 from EBook
+                    //calculate new weight and add momentum using previous delta
+                    //equation 4.22 from EBook
+                    iToHWeights[i, j] += delta + alpha * iToHPrevDeltaWeights[i, j];
+                }
+            }
+
+            // 3b. update input to hidden biases
+            for (int i = 0; i < iToHBiases.Length; ++i)
+            {
+                double delta = eta * hiddenGrads[i] * 1.0; // the 1.0 is the constant input for any bias
+                iToHBiases[i] += delta;
+                iToHBiases[i] += alpha * iToHPrevDeltaBiases[i];
+            }
+
+            // 4. update hidden to output weights
+            for (int i = 0; i < hToOutWeights.GetLength(0); ++i)  // 0..3 (4)
+            {
+                for (int j = 0; j < hToOutWeights.GetLength(1); ++j) // 0..1 (2)
+                {
+                    double delta = eta * outGrads[j] * iToHOutputs[i];  // see above: ihOutputs are inputs to next layer
+                    hToOutWeights[i, j] += delta;
+                    hToOutWeights[i, j] += alpha * hToOutPrevDeltaWeights[i, j];
+                    hToOutPrevDeltaWeights[i, j] = delta;
+                }
+            }
+
+            // 4b. update hidden to output biases
+            for (int i = 0; i < hToOutBiases.Length; ++i)
+            {
+                double delta = eta * outGrads[i] * 1.0;
+                hToOutBiases[i] += delta;
+                hToOutBiases[i] += alpha * hToOutPrevDeltaBiases[i];
+                hToOutPrevDeltaBiases[i] = delta;
+            }
+        } // UpdateWeights
+
+        public void SetWeights(double[] weights)
+        {
+            // copy weights and biases in weights[] array to i-h weights, i-h biases, h-o weights, h-o biases
+            int WeightsCount = (inputCount * hiddenCount) + (hiddenCount * outputCount) + hiddenCount + outputCount;
+
+            int k = 0; // points into weights param
+
+            for (int i = 0; i < inputCount; ++i)
+                for (int j = 0; j < hiddenCount; ++j)
+                    iToHWeights[i, j] = weights[k++];
+
+            for (int i = 0; i < hiddenCount; ++i)
+                iToHBiases[i] = weights[k++];
+
+            for (int i = 0; i < hiddenCount; ++i)
+                for (int j = 0; j < outputCount; ++j)
+                    hToOutWeights[i, j] = weights[k++];
+
+            for (int i = 0; i < outputCount; ++i)
+                hToOutBiases[i] = weights[k++];
+        }
+
+        public double[] GetWeights()
+        {
+            //to calculate total number of weights and biases
+            int weightsCount = (inputCount * hiddenCount) + (hiddenCount * outputCount) + hiddenCount + outputCount;
+            double[] result = new double[weightsCount];
+            int k = 0;
+            for (int i = 0; i < iToHWeights.GetLength(0); ++i)
+                for (int j = 0; j < iToHWeights.GetLength(1); ++j)
+                    result[k++] = iToHWeights[i, j];
+            for (int i = 0; i < iToHBiases.Length; ++i)
+                result[k++] = iToHBiases[i];
+            for (int i = 0; i < hToOutWeights.GetLength(0); ++i)
+                for (int j = 0; j < hToOutWeights.GetLength(1); ++j)
+                    result[k++] = hToOutWeights[i, j];
+            for (int i = 0; i < hToOutBiases.Length; ++i)
+                result[k++] = hToOutBiases[i];
+            return result;
+        }
+
+        public double[] ComputeOutputs(double[] xValues)
+        {
+            for (int i = 0; i < hiddenCount; ++i)
+                iToHSums[i] = 0.0;
+            for (int i = 0; i < outputCount; ++i)
+                hToOutSums[i] = 0.0;
+
+            for (int i = 0; i < xValues.Length; ++i) // copy x-values to inputs
+                this.inputs[i] = xValues[i];
+
+            for (int j = 0; j < hiddenCount; ++j)  // compute input-to-hidden weighted sums
+                for (int i = 0; i < inputCount; ++i)
+                    iToHSums[j] += this.inputs[i] * iToHWeights[i, j];
+
+            for (int i = 0; i < hiddenCount; ++i)  // add biases to input-to-hidden sums
+                iToHSums[i] += iToHBiases[i];
+
+            for (int i = 0; i < hiddenCount; ++i)   // determine input-to-hidden output
+                iToHOutputs[i] = SigmoidFunction(iToHSums[i]);
+
+            for (int j = 0; j < outputCount; ++j)   // compute hidden-to-output weighted sums
+                for (int i = 0; i < hiddenCount; ++i)
+                    hToOutSums[j] += iToHOutputs[i] * hToOutWeights[i, j];
+
+            for (int i = 0; i < outputCount; ++i)  // add biases to input-to-hidden sums
+                hToOutSums[i] += hToOutBiases[i];
+
+            for (int i = 0; i < outputCount; ++i)   // determine hidden-to-output result
+                this.outputs[i] = HyperTanFunction(hToOutSums[i]);
+
+            double[] result = new double[outputCount]; // could define a GetOutputs method instead
+            this.outputs.CopyTo(result, 0);
+
+            return result;
+        } // ComputeOutputs
+
+        private static double StepFunction(double x) // an activation function that isn't compatible with back-propagation bcause it isn't differentiable
+        {
+            if (x > 0.0) return 1.0;
+            else return 0.0;
+        }
+
+        private static double SigmoidFunction(double x)
+        {
+            if (x < -45.0) return 0.0;
+            else if (x > 45.0) return 1.0;
+            else return 1.0 / (1.0 + Math.Exp(-x));
+        }
+
+        private static double HyperTanFunction(double x)
+        {
+            if (x < -10.0) return -1.0;
+            else if (x > 10.0) return 1.0;
+            else return Math.Tanh(x);
+        }
+    }
+}
---
+++ BackPropagationNNClass_V3/BackPropagationNNClass_V3/MainWindow.xaml.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace BackPropagationNNClass_V3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Thread thread = null;
+
+        double[,] pattern = null;
+
+        private void btn_begintrain_Click(object sender, RoutedEventArgs e)
+        {
+            lsbx_output0.Items[3] = "Desired:" + txb_desiredoutput0.Text;
+            lsbx_output1.Items[3] = "Desired:" + txb_desiredoutput1.Text;
+
+            txb_input0.Text = txb_inputpattern0.Text;
+            txb_input1.Text = txb_inputpattern1.Text;
+            txb_input2.Text = txb_inputpattern2.Text;
+
+            btn_begintrain.IsEnabled = false;
+
+            double[] values = new double[]{ double.Parse(txb_inputpattern0.Text), double.Parse(txb_inputpattern1.Text), double.Parse(txb_inputpattern2.Text),
+                double.Parse(txb_desiredoutput0.Text), double.Parse(txb_desiredoutput1.Text) };
+
+            thread = new Thread(new ThreadStart(Run));
+            //in order to terminate the thread as soon as the WPF Window closed 
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        //"Creating a 3-input, 4-hidden, 2-output neural network
+        //Using sigmoid function for input-to-hidden activation
+        //Using tanh function for hidden-to-output activation
+        ANeuralNetwork nn = new ANeuralNetwork(3, 4, 2);
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            initScreen();
+            // arbitrary weights and biases
+            //double[] weights = new double[] {
+            //      0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2,
+            //      -2.0, -6.0, -1.0, -7.0,
+            //      1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
+            //      -2.5, -5.0 };
+
+            double[] weights = new double[] {
+                  0.01, 0.12, 0.3, 0.4, 0.5, 0.6, 0.2, 0.5, 0.09, 0.1, 2.1, 0.2,
+                  3.0, 6.0, -1.0, -6.0,
+                  1.3, 2.4, 1.5, 0.6, 1.7, 1.8, 1.9, 2.0,
+                  -3.5, 3.5 };
+
+            //Loading neural network weights and biases
+            nn.SetWeights(weights);
+
+            pattern = new double[4, 5] { 
+                                        { 1.0, 0.0, 1.0, 1.0, 0.0 },
+                                        { 0.0, 1.0, 0.0, 0.0, 1.0 },
+                                        { 1.0, 1.0, 1.0, 1.0, 1.0 },
+                                        { 1.0, 1.0, 0.0, 0.0, 0.0 },
+                                        };
+        }
+
+        private void initScreen()
+        {
+            lsbx_hidden0.Items.Add("Bias:0.0");
+            lsbx_hidden0.Items.Add("In:0.0");
+            lsbx_hidden0.Items.Add("Out:0.0");
+
+            lsbx_hidden1.Items.Add("Bias:0.0");
+            lsbx_hidden1.Items.Add("In:0.0");
+            lsbx_hidden1.Items.Add("Out:0.0");
+
+            lsbx_hidden2.Items.Add("Bias:0.0");
+            lsbx_hidden2.Items.Add("In:0.0");
+            lsbx_hidden2.Items.Add("Out:0.0");
+
+            lsbx_hidden3.Items.Add("Bias:0.0");
+            lsbx_hidden3.Items.Add("In:0.0");
+            lsbx_hidden3.Items.Add("Out:0.0");
+
+            lsbx_output0.Items.Add("Bias:0.0");
+            lsbx_output0.Items.Add("In:0.0");
+            lsbx_output0.Items.Add("Out:0.0");
+            lsbx_output0.Items.Add("Desired:0.0");
+            lsbx_output0.Items.Add("Error:0.0");
+
+            lsbx_output1.Items.Add("Bias:0.0");
+            lsbx_output1.Items.Add("In:0.0");
+            lsbx_output1.Items.Add("Out:0.0");
+            lsbx_output1.Items.Add("Desired:0.0");
+            lsbx_output1.Items.Add("Error:0.0");
+        }
+
+
+        static double Error(double[] target, double[] output) // sum absolute error. could put into NeuralNetwork class.
+        {
+            double sum = 0.0;
+            for (int i = 0; i < target.Length; ++i)
+                sum += Math.Abs(target[i] - output[i]);
+            return sum;
+        }
+
+        private void Run()
+        {
+            try
+            {
+                Random rnd = new Random();
+                uint ctr = 0;
+                while (ctr < 1000)
+                {
+                    ctr++;
+                    //for (int i = 0; i < pattern.GetLength(0); i++)
+                    //{
+                    int i = rnd.Next(0, pattern.GetLength(0));
+                    //Setting inputs:
+                    double[] xValues = new double[] { pattern[i, 0], pattern[i, 1], pattern[i, 2] };
+
+                    //double[] initialOutputs = nn.ComputeOutputs(xValues);
+
+                    double[] tValues = new double[] { pattern[i, 3], pattern[i, 4] }; // target (desired) values. note these only make sense for tanh output activation
+
+                    double eta = 0.90;  // learning rate - controls the maginitude of the increase in the change in weights. found by trial and error.
+                    double alpha = 0.04; // momentum - to discourage oscillation. found by trial and error.
+
+                    //Entering main back-propagation compute-update cycle
+                    //Stopping when sum absolute error <= 0.01 or 1,000 iterations
+
+                    double[] yValues = nn.ComputeOutputs(xValues); // prime the back-propagation loop
+                    double error = Error(tValues, yValues);
+                    //to update the Label from an thread we have to delegate
+                    //the update to UIthread...
+                    lbl_iterations.Dispatcher.Invoke(
+                          System.Windows.Threading.DispatcherPriority.Normal,
+                          new Action(
+                            delegate()
+                            {
+                                lbl_iterations.Content = "Trainning Iterations: " + ctr;
+                            }
+                        ));
+
+                    //Updating weights and biases using back-propagation
+                    nn.UpdateWeights(tValues, eta, alpha);
+                    //Computing new outputs:
+                    yValues = nn.ComputeOutputs(xValues);
+
+                    //Computing new error
+                    error = Error(tValues, yValues);
+                    //Console.WriteLine("Error = " + error.ToString("F4"));
+                    ActualizeLabel(lbl_totalerror, "Total Error: " + error);
+                    //}
+                }
+
+                //ActualizeListBox(lsbx_output0, "Error:" + (tValues[0] - yValues[0]), 4);
+                //ActualizeListBox(lsbx_output1, "Error:" + (tValues[1] - yValues[1]), 4);
+
+                //Best weights and biases found:
+                double[] bestWeights = nn.GetWeights();
+                //Helpers.ShowVector(bestWeights, 2, true);
+                ShowWeights(bestWeights);
+                //to update the Button from an thread we have to delegate
+                //the update to UIthread...
+                btn_begintrain.Dispatcher.Invoke(
+                      System.Windows.Threading.DispatcherPriority.Normal,
+                      new Action(
+                        delegate()
+                        {
+                            btn_begintrain.IsEnabled = true;
+                        }
+                    ));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fatal: " + ex.Message);
+            }
+
+        }
+
+        private void ShowWeights(double[] weights)
+        {
+            ActualizeTextBox(txb_weight00, weights[0].ToString());
+            ActualizeTextBox(txb_weight10, weights[1].ToString());
+            ActualizeTextBox(txb_weight20, weights[2].ToString());
+            ActualizeTextBox(txb_weight30, weights[3].ToString());
+
+            ActualizeTextBox(txb_weight01, weights[4].ToString());
+            ActualizeTextBox(txb_weight11, weights[5].ToString());
+            ActualizeTextBox(txb_weight21, weights[6].ToString());
+            ActualizeTextBox(txb_weight31, weights[7].ToString());
+
+            ActualizeTextBox(txb_weight02, weights[8].ToString());
+            ActualizeTextBox(txb_weight12, weights[9].ToString());
+            ActualizeTextBox(txb_weight22, weights[10].ToString());
+            ActualizeTextBox(txb_weight32, weights[11].ToString());
+
+            ActualizeListBox(lsbx_hidden0, "Bias:" + weights[12], 0);
+            ActualizeListBox(lsbx_hidden1, "Bias:" + weights[13], 0);
+            ActualizeListBox(lsbx_hidden2, "Bias:" + weights[14], 0);
+            ActualizeListBox(lsbx_hidden3, "Bias:" + weights[15], 0);
+
+            ActualizeTextBox(txb_outweight00, weights[16].ToString());
+            ActualizeTextBox(txb_outweight10, weights[17].ToString());
+
+            ActualizeTextBox(txb_outweight01, weights[18].ToString());
+            ActualizeTextBox(txb_outweight11, weights[19].ToString());
+
+            ActualizeTextBox(txb_outweight02, weights[20].ToString());
+            ActualizeTextBox(txb_outweight12, weights[21].ToString());
+
+            ActualizeTextBox(txb_outweight03, weights[22].ToString());
+            ActualizeTextBox(txb_outweight13, weights[23].ToString());
+
+            ActualizeListBox(lsbx_output0, "Bias:" + weights[24], 0);
+            ActualizeListBox(lsbx_output1, "Bias:" + weights[25], 0);
+        }
+
+        private void ActualizeListBox(ListBox lsbx, string msg, int i)
+        {
+            //to update the TextBox from an thread we have to delegate
+            //the update to UIthread...
+            btn_begintrain.Dispatcher.Invoke(
+                  System.Windows.Threading.DispatcherPriority.Normal,
+                  new Action(
+                    delegate()
+                    {
+                        lsbx.Items[i] = msg;
+                    }
+                ));
+
+        }
+
+        private void ActualizeTextBox(TextBox txb, string msg)
+        {
+            //to update the Label from an thread we have to delegate
+            //the update to UIthread...
+            btn_begintrain.Dispatcher.Invoke(
+                  System.Windows.Threading.DispatcherPriority.Normal,
+                  new Action(
+                    delegate()
+                    {
+                        txb.Text = msg;
+                    }
+                ));
+        }
+
+        private void ActualizeLabel(Label lbl, string msg)
+        {
+            //to update the Label from an thread we have to delegate
+            //the update to UIthread...
+            btn_begintrain.Dispatcher.Invoke(
+                  System.Windows.Threading.DispatcherPriority.Normal,
+                  new Action(
+                    delegate()
+                    {
+                        lbl.Content = msg;
+                    }
+                ));
+        }
+
+        private void btn_endtrain_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ActualizeWeightBias()
+        {
+        }
+
+        private void btn_calculate_Click(object sender, RoutedEventArgs e)
+        {
+            //Setting inputs:
+            double[] xValues = new double[] { double.Parse(txb_input0.Text), double.Parse(txb_input1.Text), double.Parse(txb_input2.Text) };
+
+            double[] initialOutputs = nn.ComputeOutputs(xValues);
+
+            double[] tValues = new double[] { double.Parse(txb_desiredout0.Text), double.Parse(txb_desiredout1.Text) }; // target (desired) values. note these only make sense for tanh output activation
+
+            double[] yValues = nn.ComputeOutputs(xValues); // prime the back-propagation loop
+
+            ActualizeListBox(lsbx_output0, "Error:" + (tValues[0] - yValues[0]), 4);
+            ActualizeListBox(lsbx_output1, "Error:" + (tValues[1] - yValues[1]), 4);
+
+            //Computing new error
+            double error = Error(tValues, yValues);
+            //Console.WriteLine("Error = " + error.ToString("F4"));
+            lbl_totalerrorOut.Content = "Total Error: " + error;
+        }
+    }
+}
